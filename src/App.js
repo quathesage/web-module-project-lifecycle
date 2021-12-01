90b01f2f7a448c0a9b9bd26529aae3b9f11b1567
Qua Wilburn <quawilburn@Quas-MacBook-Air.local>
2021-11-30 20:49:37 -0600

    Created mock up of dom

--- src/App.js
+++ src/App.js
@@ -2,10 +2,37 @@ import React from 'react';
 
 class App extends React.Component {
   render() {
-    return(<div>
-      Github Card
-    </div>);
-  }
+    return(
+      <div>
+        <h1>Github Card</h1>
+        <form>
+          <input />
+          <button>Search</button>
+        </form>
+        <div>
+          <img src='https://avatars.githubusercontent.com/u/90537370?v=4'/>
+          <h2>DeQuavion Wilburn</h2>
+          <h4>Full-Stack Developer</h4>
+        </div>
+        <div>
+          <h2>Followers:</h2>
+        </div>
+        <div id='followers'>
+          <div>
+            <img src='https://avatars.githubusercontent.com/u/90537370?v=4'/>
+            <h3>User Name</h3>
+          </div>
+          <div>
+            <img src='https://avatars.githubusercontent.com/u/90537370?v=4'/>
+            <h3>User Name</h3>
+          </div>
+          <div>
+            <img src='https://avatars.githubusercontent.com/u/90537370?v=4'/>
+            <h3>User Name</h3>
+          </div>
+        </div>
+      </div>
+    )}
 }
 
 export default App;
